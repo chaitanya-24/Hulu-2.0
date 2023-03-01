0dc2d424a2f43bd7362d1f60b549d8237d06fd98
Chaitanya Sawant <csawant.cs7@gmail.com>
2023-03-01 12:37:55 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['links.papareact.com', 'image.tmdb.org'],
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
